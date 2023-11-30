c470244569803f0313d1501091ec9eb5b58f3625
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-11-30 15:36:51 -0600

    Inicio del codigo

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/",  methods=["GET"])
+def index():
+    return render_template('index.html')
+
+@app.route("/ver", methods=["GET", "POST"])
+def ver():
+    return render_template('ver.html')
+
+@app.route("/observar", methods=["GET"])
+def observar():
+    return render_template('observar.html')
+
+if __name__ == '__name__':
+    app.run()
